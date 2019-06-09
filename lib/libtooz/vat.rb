9f4114ba3ea476a94f4714f06ef90d1b92c0f6e2
Balazs Kutil <balazs@kutilovi.cz>
2019-06-09 10:07:23 +0200

    Přidání metody na výpočet DPH

--- lib/libtooz/vat.rb
+++ lib/libtooz/vat.rb
@@ -1,8 +1,12 @@
 require "libtooz/vat/version"
+require "libtooz/vat/country_rates"
 
 module Libtooz
   module Vat
     class Error < StandardError; end
-    # Your code goes here...
+
+    def self.compute(country:, amount:)
+      ((CountryRates[country] * amount) / 100).round
+    end
   end
 end
