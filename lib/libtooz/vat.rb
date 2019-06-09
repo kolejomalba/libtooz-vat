74df89b8cc3bee13162d508c41973ceef0d7185d
Balazs Kutil <balazs@kutilovi.cz>
2019-06-09 08:39:45 +0200

    První verze
    Generováno bundle gem libtooz-vat

---
+++ lib/libtooz/vat.rb
@@ -0,0 +1,8 @@
+require "libtooz/vat/version"
+
+module Libtooz
+  module Vat
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/libtooz/vat/version.rb
@@ -0,0 +1,5 @@
+module Libtooz
+  module Vat
+    VERSION = "0.1.0"
+  end
+end
